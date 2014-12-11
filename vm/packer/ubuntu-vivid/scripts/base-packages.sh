c468144aeb93861a5abb88c85a2d785b1e3822b0
dave <David-Schmitz@gmx.net>
2014-12-11 02:17:52 +0100

    remove openjdk8

--- vm/packer/ubuntu-vivid/scripts/base-packages.sh
+++ vm/packer/ubuntu-vivid/scripts/base-packages.sh
@@ -5,4 +5,4 @@ sudo sed -i 's/# \(.*multiverse$\)/\1/g' /etc/apt/sources.list && \
   apt-get -y upgrade && \
   apt-get install -y build-essential && \
   apt-get install -y --force-yes software-properties-common && \
-  apt-get install -y byobu cgroup-lite cgroup-bin curl git mercurial subversion htop man unzip vim wget mc zsync ssh snapper xz-utils openjdk-8-jdk openjdk-8-jre-headless
+  apt-get install -y byobu cgroup-lite cgroup-bin curl git mercurial subversion htop man unzip vim wget mc zsync ssh snapper xz-utils
