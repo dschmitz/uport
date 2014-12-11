5a931f06d335392177c304c8d935a49e7d04854e
dave <David-Schmitz@gmx.net>
2014-12-11 01:49:02 +0100

    ramp up packer to version 0.7.5

--- vm/packer/packer-install.sh
+++ vm/packer/packer-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-PACKER="packer_0.7.2_linux_amd64"
+PACKER="packer_0.7.5_linux_amd64"
 
 (cd /tmp && wget -N https://dl.bintray.com/mitchellh/packer/$PACKER.zip)
 unzip /tmp/$PACKER.zip -d /opt/$PACKER
