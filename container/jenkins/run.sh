097d314928907ea5dbbf14b2c1a34f5bfdad1e43
dave <David-Schmitz@gmx.net>
2014-12-12 12:17:54 +0100

    fix script

--- container/jenkins/run.sh
+++ container/jenkins/run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 CONTAINER=$(basename `pwd`)
-docker run -d -p 18080:8080 -p 8081:8081 --name $CONTAINER here/$CONTAINER
+docker run -d -p 18080:8080 -p 8081:8081 --name $CONTAINER uport/$CONTAINER
