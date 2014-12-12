1de40fd9c863d82e1aa3ea1d1e5fe905d1dd0930
dave <David-Schmitz@gmx.net>
2014-12-12 12:47:07 +0100

    refering to docker bridge + jenkins port to proof that swarm logic is working now we can start with sdn stuff

--- container/jenkins-swarm-slave/run.sh
+++ container/jenkins-swarm-slave/run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 CONTAINER=$(basename `pwd`)
-docker run -d --name $CONTAINER uport/$CONTAINER -disableSslVerification  #-master http://127.0.0.1:18080
+docker run -d --name $CONTAINER uport/$CONTAINER -disableSslVerification  -master http://172.17.42.1:18080
